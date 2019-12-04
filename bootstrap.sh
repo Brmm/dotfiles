de38a9ff29a32aae3ed90a9fa81e21bdf8268975
Bram Monstrey <monstrey.bram@gmail.com>
2019-12-04 14:16:15 +0100

    Use node v10 instead of v12

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,8 +24,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 12
-nvm use 12
+nvm install 10
+nvm use 10
 npm install -g eslint npm npm-check-updates create-react-native-app yarn serverless typescript prettier lerna
 
 ## Copy iTerm2 settings
